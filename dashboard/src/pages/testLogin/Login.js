21b1e95bb9d93a56167cfd929aaf4321fd0702d6
skpn <sikpenou@student.42.fr>
2020-07-15 18:17:09 +0200

    added dashboard template

---
+++ dashboard/src/pages/testLogin/Login.js
@@ -0,0 +1,127 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import { withRouter, Redirect, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Container, Alert, Button, FormGroup, Label, InputGroup, InputGroupAddon, Input, InputGroupText } from 'reactstrap';
+import Widget from '../../components/Widget';
+import { loginUser } from '../../actions/user';
+import microsoft from '../../images/microsoft.png';
+
+const Login = (props) => {
+
+    console.log(props)
+    console.log(props.dispatch)
+    const [email, setEmail] = useState('admin@flatlogic.com')
+    const [password, setPassword] = useState('password')
+
+    const propTypes = {
+        dispatch: PropTypes.func.isRequired,
+    };
+
+    const isAuthenticated = (token) => {
+        if (token) return true;
+    }
+
+    const changeEmail = (event) => {
+        setEmail(event.target.value );
+    }
+
+    const changePassword = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const doLogin = (event) => {
+        event.preventDefault();
+        props.dispatch(loginUser({ email, password}));
+    }
+
+    const signUp = () => {
+        props.history.push('/register');
+    }
+
+    const { from } = props.location.state || { from: { pathname: '/app' } }; // eslint-disable-line
+
+    // cant access login page while logged in
+    console.log(typeof(isAuthenticated))
+    if (isAuthenticated(JSON.parse(localStorage.getItem('authenticated')))) {
+        return (
+            <Redirect to={from} />
+        );
+    }
+    else {
+        return (
+            <div className="auth-page">
+                <Container>
+                    <Widget className="widget-auth mx-auto" title={<h3 className="mt-0">testLogin to your Web App</h3>}>
+                        <p className="widget-auth-info">
+                            Use your email to sign in.
+                        </p>
+                        <form onSubmit={doLogin}>
+                            {
+                                props.errorMessage && (
+                                    <Alert className="alert-sm widget-middle-overflow rounded-0" color="danger">
+                                        {props.errorMessage}
+                                    </Alert>
+                                )
+                            }
+                            <FormGroup className="mt">
+                                <Label for="email">Email</Label>
+                                <InputGroup className="input-group-no-border">
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText>
+                                            <i className="la la-user text-white"/>
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input id="email" className="input-transparent pl-3" value={email} onChange={changeEmail} type="email"
+                                        required name="email" placeholder="Email"/>
+                                </InputGroup>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="password">Password</Label>
+                                <InputGroup className="input-group-no-border">
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText>
+                                            <i className="la la-lock text-white"/>
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input id="password" className="input-transparent pl-3" value={password}
+                                        onChange={changePassword} type="password"
+                                        required name="password" placeholder="Password"/>
+                                </InputGroup>
+                            </FormGroup>
+                            <div className="bg-widget auth-widget-footer">
+                                <Button type="submit" color="danger" className="auth-btn"
+                                        size="sm" style={{color: '#fff'}}>
+                                <span className="auth-btn-circle" style={{marginRight: 8}}>
+                                    <i className="la la-caret-right"/>
+                                </span>
+                                {props.isFetching ? 'Loading...' : 'Login'}
+                                </Button>
+                                <p className="widget-auth-info mt-4">
+                                    Don't have an account? Sign up now!
+                                </p>
+                                <Link className="d-block text-center mb-4" to="register">Create an Account</Link>
+                                <div className="social-buttons">
+                                    <Button color="primary" className="social-button">
+                                        <i className="social-icon social-google"/>
+                                        <p className="social-text">GOOGLE</p>
+                                    </Button>
+                                    <Button color="success" className="social-button">
+                                        <i className="social-icon social-microsoft"
+                                        style={{backgroundImage: `url(${microsoft})`}}/>
+                                        <p className="social-text" style={{color: '#fff'}}>MICROSOFT</p>
+                                    </Button>
+                                </div>
+                            </div>
+                        </form>
+                    </Widget>
+                </Container>
+                <footer className="auth-footer">
+                    2020 &copy; Light Blue Template - React Admin Dashboard Template.
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Login;
